eca30cb8587acff6252cd18e6435797423b604e8
sclem <clem.spencer@gmail.com>
2017-11-13 15:51:54 -0500

    Package with node-pre-gyp

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-set +e
+set -e
 set -x
 
-npm install -g npm@$NPM_VERSION
-npm install -g node@$NODE_VERSION
+if [[ $(npm -v) != "$NPM_VERSION" ]]; then
+    npm install -g npm@$NPM_VERSION
+fi
+
+if [[ $(node -v) != "v$NODE_VERSION" ]]; then
+    npm install -g node@$NODE_VERSION
+fi
 npm install -g nw-gyp@latest
+npm install -g node-pre-gyp@latest
 
 npm init -y
 
-npm -v
-node -v
-nw-gyp -v
-
 npm install sqlite3 --runtime=node-webkit --target=$NWJS_VERSION --target-arch=x64 --target_platform=win32
+
+cd node_modules/sqlite3
+
+node-pre-gyp --runtime=node-webkit --target=$NWJS_VERSION package
